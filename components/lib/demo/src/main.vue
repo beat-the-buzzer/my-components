51c94650cf55c67ba92139e15b723a5a0fdb6270
wangtao <2985593303@qq.com>
2023-01-27 15:41:52 +0800

    demo

---
+++ components/lib/demo/src/main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-demo">DEMO</div>
+</template>
+
+<script>
+export default {
+  name: 'Demo'
+
+}
+</script>
+
+<style>
+
+</style>
--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '../components/css/demo.scss'
+import Demo from '../components/lib/demo/index.js'
+
+Vue.use(Demo) // Demo.install
+
 Vue.config.productionTip = false
 
 new Vue({
