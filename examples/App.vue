8bf8643d25d71579954cbd42beb63c1b1c3af2a8
wangtao <2985593303@qq.com>
2023-01-31 22:03:01 +0800

    测试card组件

--- examples/App.vue
+++ examples/App.vue
@@ -2,6 +2,23 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Demo />
+    <Card imgSrc="test1.png" summary="WebSocket入门与案例实战">
+      <template v-slot:footer>
+        <div>
+          <div class="level">中级 · 100人报名</div>
+          <div class="price">￥199.00</div>
+        </div>
+      </template>
+    </Card>
+    <br/>
+    <Card :width="500" imgSrc="test2.jpg">
+      Vue从入门到进阶
+      <template v-slot:footer>
+        <div>
+          <div class="level">中级 · 100人报名</div>
+          <div class="level">￥199.00</div>
+        </div>
+      </template></Card>
   </div>
 </template>
 
@@ -22,5 +39,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.footer {
+  padding: 0 8px;
+  font-size: 12px;
+  text-align: left;
+}
+
+.level {
+  color: #9199A1;
+  padding-bottom: 8px;
+}
+
+.price {
+  color: #f01414;
 }
 </style>
--- examples/main.js
+++ examples/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import '../components/css/demo.scss'
+import '../components/css/card.scss'
 import Demo from '../components/lib/demo/index.js'
+import Card from '../components/lib/card/index.js'
 
 Vue.use(Demo) // Demo.install
+Vue.use(Card) // Demo.install
 
 Vue.config.productionTip = false
 
